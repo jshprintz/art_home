7e4808a9aeea259d3010663a51c34d7f3bbd0d23
jshprintz <jshprintz@gmail.com>
2022-10-25 22:23:32 -0700

    rounded corners for the photo carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,50 +1,54 @@
 import Carousel from "react-bootstrap/Carousel";
-import "./Carousel.css"
+import Image from "react-bootstrap/Image";
+import "./Carousel.css";
 
 function CarouselBlock() {
   return (
     <div id="carousel-container">
-    <div id="carousel-settings">
-    <Carousel fade>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://i.imgur.com/atHxTiI.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://i.imgur.com/f5UhFPx.jpg"
-          alt="Second slide"
-        />
+      <div id="carousel-settings">
+        <Carousel fade>
+          <Carousel.Item>
+            <Image
+              rounded="true"
+              className="d-block w-100"
+              src="https://i.imgur.com/atHxTiI.jpg"
+              alt="First slide"
+            />
+            <Carousel.Caption>
+              <h3>First slide label</h3>
+              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image
+              rounded="true"
+              className="d-block w-100"
+              src="https://i.imgur.com/f5UhFPx.jpg"
+              alt="Second slide"
+            />
 
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://i.imgur.com/dTqhXHd.jpg"
-          alt="Third slide"
-        />
+            <Carousel.Caption>
+              <h3>Second slide label</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image
+              rounded="true"
+              className="d-block w-100"
+              src="https://i.imgur.com/dTqhXHd.jpg"
+              alt="Third slide"
+            />
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-    </div>
+            <Carousel.Caption>
+              <h3>Third slide label</h3>
+              <p>
+                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+              </p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        </Carousel>
+      </div>
     </div>
   );
 }
