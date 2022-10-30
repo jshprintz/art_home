c94b393bd9e6876ebcadec0e982af6cb3f59b524
jshprintz <jshprintz@gmail.com>
2022-10-30 15:12:00 -0700

    added logo to header

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,20 +1,29 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import Image from "react-bootstrap/Image";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import "./NavBar.css"
+import "./NavBar.css";
 
 function NavBar() {
   return (
-    <Navbar id="nav-bar" bg="light" expand="lg">
+    <Navbar id="nav-bar" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Art Home</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <Image
+            id="logo-nav"
+            src="https://i.imgur.com/G4eja1w.png"
+            alt="logo"
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link href="#story-buffer">Story</Nav.Link>
             <NavDropdown title="Pictures" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#bathroom-buffer">Bathroom</NavDropdown.Item>
+              <NavDropdown.Item href="#bathroom-buffer">
+                Bathroom
+              </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Bedroom</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Dining</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.4">Game</NavDropdown.Item>
