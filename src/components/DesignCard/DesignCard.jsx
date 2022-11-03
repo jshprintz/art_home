d8e338ca137bec6a817134cfd7a3d3d11697fbea
jshprintz <jshprintz@gmail.com>
2022-11-03 15:47:28 -0700

    design images more responsive now. still working on making them vertical on small screens

--- src/components/DesignCard/DesignCard.jsx
+++ src/components/DesignCard/DesignCard.jsx
@@ -2,10 +2,6 @@ import React from "react";
 import "./DesignCard.css";
 
 export default function DesignCard({ imgAlt, imgSrc }) {
-
-    //I think this needs to actually be a card instead of just an image.
-    // either that, or we utilize object-fit property and just designate a set height and width
-
   return (
     <div id="design-card">
       <img id="design-img" src={imgSrc} alt={imgAlt} />
